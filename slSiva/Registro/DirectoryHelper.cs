2c7a11f3489ee52c5129b76d958eaf8944c4b273
grecio <grecio@gmail.com>
2017-01-04 01:31:36 -0300

    Implementando software de registro

---
+++ slSiva/Registro/DirectoryHelper.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace Registro
+{
+    public static class DirectoryHelper
+    {   
+        public static IEnumerable<string> GetFiles(string path,
+                            string searchPatternExpression = "",
+                            SearchOption searchOption = SearchOption.TopDirectoryOnly)
+        {
+            Regex reSearchPattern = new Regex(searchPatternExpression, RegexOptions.IgnoreCase);
+            return Directory.EnumerateFiles(path, "*", searchOption)
+                            .Where(file =>
+                                     reSearchPattern.IsMatch(Path.GetExtension(file))).OrderByDescending(item => item);
+        }
+                
+        public static IEnumerable<string> GetFiles(string path, string[] searchPatterns, SearchOption searchOption = SearchOption.TopDirectoryOnly)
+        {
+            return searchPatterns.AsParallel()
+                   .SelectMany(searchPattern =>
+                          Directory.EnumerateFiles(path, searchPattern, searchOption)).OrderByDescending(item => item);
+        }
+    }
+}
---
+++ slSiva/Registro/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Registro
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Aplicativo Registro Software");
+
+            var arquivos = DirectoryHelper.GetFiles(@"C:\inetpub\wwwroot\projetos_git\siva\siva\slSiva", @"^.*\.(cs|cshtml|config|asax|csproj|pc)$", System.IO.SearchOption.AllDirectories);
+            
+            var quantidade = arquivos.Count();
+
+            Console.WriteLine(string.Format("Quantidade de Arquivos Encontrados : {0}", quantidade.ToString()));
+
+            var pdf = PdfCreator.GetInstance;
+
+            foreach (var arquivo in arquivos)
+            {
+                Console.WriteLine(string.Format("[{0}] PROCESSANDO ARQUIVO {1}", quantidade, arquivo));
+
+                pdf.Arquivo = arquivo;
+
+                if (pdf.Adicionar())
+                {
+                    quantidade--;
+                    continue;
+                }
+            }
+
+            pdf.Concluir();
+            pdf.Imprimir();
+
+            Console.WriteLine("Processo de Registro Concluído!");
+
+            Console.ReadKey();
+        }
+    }
+}
