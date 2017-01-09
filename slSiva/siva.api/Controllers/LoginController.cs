250fa7c29b360a24e6c9dfa1578ee5400df1f4b3
grecio <grecio@gmail.com>
2017-01-09 03:01:56 -0300

    ajustes

--- slSiva/siva.api/Controllers/LoginController.cs
+++ slSiva/siva.api/Controllers/LoginController.cs
@@ -20,21 +20,21 @@ namespace siva.api.Controllers
         [HttpPost]
         public ActionResult Entrar([System.Web.Http.FromBody]Dominio.Usuario usuario)
         {
-            //try
-            //{
-            var usuarioDb = bpUsuario.EfetuarLogin(usuario.LOGIN, usuario.SENHA);
-
-            if (usuarioDb.SQ_USUARIO > 0)
+            try
             {
-                Session["UsuarioLogado"] = usuarioDb;
-            }
+                var usuarioDb = bpUsuario.EfetuarLogin(usuario.LOGIN, usuario.SENHA);
 
-            return RedirectToAction("Index", "Principal");
-            //}
-            //catch (Exception ex)
-            //{
-            //    return RedirectToAction("Index");
-            //}            
+                if (usuarioDb.SQ_USUARIO > 0)
+                {
+                    Session["UsuarioLogado"] = usuarioDb;
+                }
+
+                return RedirectToAction("Index", "Principal");
+            }
+            catch (Exception ex)
+            {
+                return RedirectToAction("Index");
+            }
         }            
     }
 }
