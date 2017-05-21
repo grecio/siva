1cc27b714c86d0f4a8722b1768bf78fbe5073df9
grecio <grecio@gmail.com>
2017-05-21 19:02:52 -0300

    Ajustei o subsistema de coleta do codigo fonte para registro.

--- slSiva/Registro/Program.cs
+++ slSiva/Registro/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,31 +11,37 @@ namespace Registro
     {
         static void Main(string[] args)
         {
+
+            var mydocpath = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
+            var sb = new StringBuilder();
+
             Console.WriteLine("Aplicativo Registro Software");
 
             var arquivos = DirectoryHelper.GetFiles(@"C:\inetpub\wwwroot\projetos_git\siva\siva\slSiva", @"^.*\.(cs|cshtml|config|asax|csproj|pc)$", System.IO.SearchOption.AllDirectories);
             
             var quantidade = arquivos.Count();
 
-            Console.WriteLine(string.Format("Quantidade de Arquivos Encontrados : {0}", quantidade.ToString()));
-
-            var pdf = PdfCreator.GetInstance;
+            Console.WriteLine(string.Format("Quantidade de Arquivos Encontrados : {0}", quantidade.ToString()));            
 
             foreach (var arquivo in arquivos)
             {
                 Console.WriteLine(string.Format("[{0}] PROCESSANDO ARQUIVO {1}", quantidade, arquivo));
 
-                pdf.Arquivo = arquivo;
-
-                if (pdf.Adicionar())
+                using (var sr = new StreamReader(arquivo))
                 {
-                    quantidade--;
-                    continue;
+                    sb.Append(sr.ReadToEnd());
+                    sb.AppendLine();
+                    sb.AppendLine();
                 }
+
+                quantidade--;
+               
             }
 
-            pdf.Concluir();
-            pdf.Imprimir();
+            using (var outfile = new StreamWriter(mydocpath + @"\siva-registro-software.txt"))
+            {
+                outfile.Write(sb.ToString());
+            }
 
             Console.WriteLine("Processo de Registro Concluído!");
 
